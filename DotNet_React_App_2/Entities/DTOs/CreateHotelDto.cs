85f961b3b2e0b2f974a2c35097780a7de06265d9
tapan0810 <arceus0810@gmail.com>
2026-04-07 20:04:08 +0530

    Add project files.

---
+++ DotNet_React_App_2/Entities/DTOs/CreateHotelDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet_React_App_2.Entities.DTOs
+{
+    public class CreateHotelDto
+    {
+        public string HotelName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
