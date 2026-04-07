a53e86e59e8a235e9a9a40535acebbed9ba78e9c
tapan0810 <arceus0810@gmail.com>
2026-04-07 21:36:50 +0530

    feat: Implemented AutoMapping

--- DotNet_React_App_2/Mapping/HotelMappingProfile.cs
+++ DotNet_React_App_2/Mapping/HotelMappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using DotNet_React_App_2.Entities.DTOs;
+using DotNet_React_App_2.Entities.Models;
 
 namespace DotNet_React_App_2.Mapping
 {
@@ -6,6 +8,13 @@ namespace DotNet_React_App_2.Mapping
     {
         public HotelMappingProfile()
         {
+            CreateMap<Hotel, GetAllHotelsDto>();
+
+            CreateMap<CreateHotelDto, Hotel>();
+
+            CreateMap<CreateHotelDto,Hotel>();
+
+            CreateMap<Hotel, GetHotelByIdDto>();
 
         }
     }
