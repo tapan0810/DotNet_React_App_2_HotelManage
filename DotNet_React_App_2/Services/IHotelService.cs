2fabf2288d69fb8c1be9e67f5784652442a3631b
tapan0810 <arceus0810@gmail.com>
2026-04-08 21:09:43 +0530

    feat: Implemented HotelSevice

---
+++ DotNet_React_App_2/Services/IHotelService.cs
@@ -0,0 +1,13 @@
+﻿using DotNet_React_App_2.Entities.DTOs;
+
+namespace DotNet_React_App_2.Services
+{
+    public interface IHotelService
+    {
+        public Task<List<GetAllHotelsDto>> GetAllHotelsAsync();
+        public Task<GetHotelByIdDto> GetHotelByIdAsync(int id);
+        public Task<GetHotelByIdDto?> CreateHotel(CreateHotelDto dto);
+        public Task<bool> UpdateHotelAsync(int id,UpdateHotelDto dto);
+        public Task<bool> DeleteHotelAsync(int id);
+    }
+}
